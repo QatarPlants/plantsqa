848c3d7a72a0f5fe1d000a9b7dd2cc52a5408bbe
QatarPlants <projectgithub75@gmail.com>
2022-04-03 21:59:29 +0300

    .

---
+++ src/hello.js
@@ -0,0 +1,19 @@
+import amna from './plan.js'
+
+
+//HTML FILE 
+function Hello() {
+    return (
+      <div className="App">
+        <div id='abc'>
+            <h1>
+                hello
+            </h1>
+        </div>
+        <button onClick={amna()}> Click</button>
+      </div>
+    );
+  }
+  
+  export default Hello;
+  
---
+++ src/plan.js
@@ -0,0 +1,7 @@
+function amna(){
+    var hellooo = document.getElementById('abc')
+    hellooo.innerHTML('Am from main js')
+}
+
+
+module.exports = amna;
